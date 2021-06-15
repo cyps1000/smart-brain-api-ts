bef54b3e6c6bf20aedaee3288cc0c62c95198822
unknown <cypsciprian@gmail.com>
2021-06-15 16:41:08 +0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import express from "express";
  */
 import dotenv from "dotenv";
 import helmet from "helmet";
+import cors from "cors";
 
 /**
  * Imports routes
@@ -29,6 +30,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
+app.use(cors());
 
 /**
  * Auth Router
